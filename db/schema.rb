597d57a7f596d525ad828b4b3837cc7c42f7f1db
Cristian Carreño <kiel@acid.cl>
2013-01-29 10:03:54 -0300

    Add Book migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130129130215) do
+
+  create_table "books", :force => true do |t|
+    t.string   "tittle"
+    t.string   "author"
+    t.datetime "date"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
