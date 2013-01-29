55e2a1db8702a29eb0837aa0c3483b8e4ce57cd4
Cristian Carreño <kiel@acid.cl>
2013-01-29 09:58:28 -0300

    Add BooksController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Mybook::Application.routes.draw do
+  get "books/index"
+
+  get "books/show"
+
+  get "books/new"
+
+  get "books/create"
+
+  get "books/edit"
+
+  get "books/delete"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +60,7 @@ Mybook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'books#index'
 
   # See how all your routes lay out with "rake routes"
 
